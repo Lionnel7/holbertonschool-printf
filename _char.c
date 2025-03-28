a174b7116fb12de2473405daeee73d2e2a42ae6c
Lionnel7 <pierrerichan@gmail.com>
2025-03-28 11:39:47 +0100

    Add function to print a character in char.c

--- _char.c
+++ _char.c
@@ -2,13 +2,13 @@
 
 /**
  * _char - write the char c to stdout
- * @args: The char to print 
+ * @args: The char to print
  *
- * Return: Number of characters printed 
+ * Return: Number of characters printed
  */
 int _char(va_list args)
 {
-	char c = va_arg(args, int);
+        char c = va_arg(args, int);
 
-	return (_putchar(c));
-}	
+        return (_putchar(c));
+}
